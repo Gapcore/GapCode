a0cbaa774aa5f76f163e4dcffdab2f829ee3e520
Raphael <raphael.alonso.batista@gmail.comgit config --global user.email raphael.alonso.batista@gmail.com>
2026-06-03 19:46:26 -0300

    banco de dados

--- contato.js
+++ contato.js
@@ -1,39 +1,59 @@
+const supabaseUrl = "https://ddfvcqgzcrbchjicchty.supabase.co";
+
+const supabaseKey =
+"sb_publishable_XPWIazDX5IDMjKOcYbNtvg_-KCI0bH3";
+
+const supabaseClient = supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
+
 document
-  .getElementById("contatoForm")
-  .addEventListener("submit", function (e) {
+.getElementById("contatoForm")
+.addEventListener("submit", async (e) => {
 
     e.preventDefault();
 
+    const botao = document.querySelector("button");
+
+    botao.disabled = true;
+    botao.innerText = "Enviando...";
+
     const lead = {
-      nome: document.getElementById("nome").value,
-      email: document.getElementById("email").value,
-      telefone: document.getElementById("telefone").value,
-      mensagem: document.getElementById("mensagem").value
+        nome: document.getElementById("nome").value,
+        email: document.getElementById("email").value,
+        telefone: document.getElementById("telefone").value,
+        mensagem: document.getElementById("mensagem").value
     };
+
+    const { error } = await supabaseClient
+        .from("leads")
+        .insert([lead]);
+
+    if(error){
+
+        console.error(error);
+
+        alert("Erro ao enviar contato.");
+
+        botao.disabled = false;
+        botao.innerText = "Enviar Contato";
+
+        return;
+    }
+
+  document
+.getElementById("successCard")
+.classList.add("show");
+
+    document.getElementById("contatoForm").reset();
 
     document
-  .getElementById("contatoForm")
-  .addEventListener("submit", async function (e) {
+.getElementById("contatoForm")
+.style.display = "none";
 
-    e.preventDefault();
+document.querySelector(".titulo").style.display = "none";
 
-    const lead = {
-      nome: document.getElementById("nome").value,
-      email: document.getElementById("email").value,
-      telefone: document.getElementById("telefone").value,
-      mensagem: document.getElementById("mensagem").value
-    };
-
-    const resposta = await fetch("http://localhost:3000/leads", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(lead)
-    });
-
-    const dados = await resposta.json();
-
-    alert(dados.mensagem);
-});
+    botao.disabled = false;
+    botao.innerText = "Enviar Contato";
 });
